4c378ebf7a042492ddc684a89e46840ac026b8ba
syuilo <syuilotan@yahoo.co.jp>
2019-05-10 13:59:33 +0900

    Update index.ts

--- src/modules/maze/index.ts
+++ src/modules/maze/index.ts
@@ -158,7 +158,7 @@ export default class extends Module {
 	@autobind
 	private async post() {
 		const now = new Date();
-		if (now.getHours() !== 11) return;
+		if (now.getHours() !== 15) return;
 		const date = `${now.getFullYear()}-${now.getMonth()}-${now.getDate()}`;
 		const data = this.getData();
 		if (data.lastPosted == date) return;
