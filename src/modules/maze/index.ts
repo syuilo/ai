507b700a5cae6dc6af198851016b028aec67aef5
syuilo <syuilotan@yahoo.co.jp>
2019-05-10 13:50:19 +0900

    :art:

--- src/modules/maze/index.ts
+++ src/modules/maze/index.ts
@@ -101,11 +101,47 @@ const themes = [{
 	road: '#6A77A4',
 	marker: '#E6E5E3',
 }, {
-	bg1: '#194035',
-	bg2: '#417334',
-	wall: '#594020',
-	road: '#A67926',
-	marker: '#2E4D59',
+	bg1: '#BFD962',
+	bg2: '#EAF2AC',
+	wall: '#1E4006',
+	road: '#BFD962',
+	marker: '#74A608',
+}, {
+	bg1: '#C0CCB8',
+	bg2: '#FFE2C0',
+	wall: '#664A3C',
+	road: '#FFCB99',
+	marker: '#E78F72',
+}, {
+	bg1: '#101010',
+	bg2: '#151515',
+	wall: '#909090',
+	road: '#202020',
+	marker: '#606060',
+}, {
+	bg1: '#e0e0e0',
+	bg2: '#f2f2f2',
+	wall: '#a0a0a0',
+	road: '#e0e0e0',
+	marker: '#707070',
+}, {
+	bg1: '#7DE395',
+	bg2: '#D0F3CF',
+	wall: '#349D9E',
+	road: '#7DE395',
+	marker: '#56C495',
+}, {
+	bg1: '#C9EEEA',
+	bg2: '#DBF4F1',
+	wall: '#4BC6B9',
+	road: '#C9EEEA',
+	marker: '#19A89D',
+}, {
+	bg1: '#1e231b',
+	bg2: '#27331e',
+	wall: '#67b231',
+	road: '#385622',
+	marker: '#78d337',
 }];
 
 export default class extends Module {
