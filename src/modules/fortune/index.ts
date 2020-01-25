9d05f9794295a6c11d0a9d816f0af10e1ad8d266
syuilo <Syuilotan@yahoo.co.jp>
2020-01-26 05:21:25 +0900

    :v:

--- src/modules/fortune/index.ts
+++ src/modules/fortune/index.ts
@@ -3,7 +3,7 @@ import Module from '../../module';
 import Message from '../../message';
 import serifs from '../../serifs';
 import * as seedrandom from 'seedrandom';
-import { blessing, itemPrefixes, items } from './vocabulary';
+import { blessing, itemPrefixes, items, and } from './vocabulary';
 
 export default class extends Module {
 	public readonly name = 'fortune';
@@ -22,9 +22,15 @@ export default class extends Module {
 			const seed = `${date.getFullYear()}/${date.getMonth()}/${date.getDate()}@${msg.userId}`;
 			const rng = seedrandom(seed);
 			const omikuji = blessing[Math.floor(rng() * blessing.length)];
-			const itemPrefix = Math.floor(rng() * 5) != 0 ? itemPrefixes[Math.floor(rng() * itemPrefixes.length)] : '';
-			const item = items[Math.floor(rng() * items.length)];
-			msg.reply(`**${omikuji}🎉**\nラッキーアイテム: ${itemPrefix}${item}`, serifs.fortune.cw(msg.friend.name));
+			let item = '';
+			if (Math.floor(rng() * 5) !== 0) item += itemPrefixes[Math.floor(rng() * itemPrefixes.length)];
+			item += items[Math.floor(rng() * items.length)];
+			if (Math.floor(rng() * 3) === 0) {
+				item += and[Math.floor(rng() * and.length)];
+				if (Math.floor(rng() * 5) !== 0) item += itemPrefixes[Math.floor(rng() * itemPrefixes.length)];
+				item += items[Math.floor(rng() * items.length)];
+			}
+			msg.reply(`**${omikuji}🎉**\nラッキーアイテム: ${item}`, serifs.fortune.cw(msg.friend.name));
 			return true;
 		} else {
 			return false;
