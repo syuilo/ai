1946670f2cda52ba10fa5e9dada2e22712f6be74
syuilo <syuilotan@yahoo.co.jp>
2020-10-31 12:27:28 +0900

    nanka

--- src/modules/emoji-react/index.ts
+++ src/modules/emoji-react/index.ts
@@ -28,7 +28,7 @@ export default class extends Module {
 
 		const react = async (reaction: string, immediate = false) => {
 			if (!immediate) {
-				await delay(1500);
+				await delay(Math.random() * 1000 * 60);
 			}
 			this.ai.api('notes/reactions/create', {
 				noteId: note.id,
