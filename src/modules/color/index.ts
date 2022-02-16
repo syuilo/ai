d23ffe197c3d49e06d97df28ae78c18660fe8fe0
na2na <49822810+na2na-p@users.noreply.github.com>
2022-02-17 01:03:16 +0900

    単色の画像を生成してリプライで返すように

--- src/modules/color/index.ts
+++ src/modules/color/index.ts
@@ -1,6 +1,7 @@
 import autobind from 'autobind-decorator';
 import Module from '@/module';
 import Message from '@/message';
+import { generateColorSample } from './render';
 
 export default class extends Module {
 	public readonly name = 'color';
@@ -21,13 +22,31 @@ export default class extends Module {
             const b = Math.floor(Math.random() * 256);
             // rgbをhexに変換する
             const hex = `${r.toString(16)}${g.toString(16)}${b.toString(16)}`;
-            const message = `RGB: ${r}, ${g}, ${b} (#${hex})とかどう？ [参考](https://www.colorhexa.com/${hex})`
-			msg.reply(message, {
-				immediate: true
-			});
-			return true;
+            const message = `RGB: ${r}, ${g}, ${b} (# ${hex})とかどう？`
+
+			setTimeout(async () => {
+				const file = await this.getColorSampleFile(r,g,b);
+				this.log('Replying...');
+				msg.reply(message, { file });
+			}, 500);
+			return {
+				reaction: 'like'
+			};
 		} else {
 			return false;
 		}
+	}
+
+	@autobind
+	private async getColorSampleFile(r,g,b): Promise<any> {
+		const colorSample = generateColorSample(r,g,b);
+
+		this.log('Image uploading...');
+		const file = await this.ai.upload(colorSample, {
+			filename: 'color.png',
+			contentType: 'image/png'
+		});
+
+		return file;
 	}
 }
