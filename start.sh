180d93e618aa03c147d163094fdc9e5097eddd3b
Masaya Suzuki <suzukimasaya428@gmail.com>
2023-03-04 07:56:16 +0900

    Revert "if文修正"
    This reverts commit 38fc0c54ddf086fc08aa8157a658b86a3774ad57.

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if (grep keywordEnabled config.json | grep -q true) && (! which mecab) && [ ! -e '/usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd/' ]; then
+if (grep keywordEnabled config.json | grep -q true) && (! which mecab) && [ -e '/usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd/' ]; then
   echo "You must install MeCab and mecab-ipadic-neologd if keywordEnabled is true."
   exit 1
 fi
