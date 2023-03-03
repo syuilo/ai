49d0929c5a453a6f028f6836ae8f9ec8c9838245
Masaya Suzuki <suzukimasaya428@gmail.com>
2023-03-04 07:53:14 +0900

    mecab-ipadic-neologdのチェック追加

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-if (grep keywordEnabled config.json | grep -q true) && (! which mecab); then
-  echo "You must install MeCab if keywordEnabled is true."
+if (grep keywordEnabled config.json | grep -q true) && (! which mecab) && [ -e '/usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd/' ]; then
+  echo "You must install MeCab and mecab-ipadic-neologd if keywordEnabled is true."
   exit 1
 fi
 
