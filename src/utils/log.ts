39cda1cac4242d19dc6423b1e89ca758861e4234
Masaya Suzuki <suzukimasaya428@gmail.com>
2023-03-14 07:09:12 +0900

    fix: ログ出力に日付追加

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -2,7 +2,7 @@ import * as chalk from 'chalk';
 
 export default function(msg: string) {
 	const now = new Date();
-	const date = `${zeroPad(now.getHours())}:${zeroPad(now.getMinutes())}:${zeroPad(now.getSeconds())}`;
+	const date = `${now.getFullYear()}-${zeroPad(now.getMonth() + 1)}-${zeroPad(now.getDate())} ${zeroPad(now.getHours())}:${zeroPad(now.getMinutes())}:${zeroPad(now.getSeconds())}`;
 	console.log(`${chalk.gray(date)} ${msg}`);
 }
 
