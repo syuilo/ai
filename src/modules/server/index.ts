b0d739b26ea7f2ead0e296a1444d207e3a571fa2
n1lsqn <owner@n1l.dev>
2023-11-13 22:29:46 +0900

    fix: 出力削除

--- src/modules/server/index.ts
+++ src/modules/server/index.ts
@@ -38,6 +38,7 @@ export default class extends Module {
 	@autobind
 	private check() {
 		const average = (arr) => arr.reduce((a, b) => a + b) / arr.length;
+
 		const cpuPercentages = this.statsLogs.map(s => s && (s.cpu_usage || s.cpu) * 100 || 0);
 		const cpuPercentage = average(cpuPercentages);
 		if (cpuPercentage >= 70) {
