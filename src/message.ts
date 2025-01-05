d025458d51f109138dec676af65bfa5a246f0f63
tetsuya-ki <64536338+tetsuya-ki@users.noreply.github.com>
2025-01-05 17:55:04 +0900

    fix syuilo-issue #136
    - DMのときはDMで返信するよう変更

--- src/message.ts
+++ src/message.ts
@@ -83,13 +83,20 @@ export default class Message {
 			await sleep(2000);
 		}
 
-		return await this.ai.post({
+		const postData = {
 			replyId: this.note.id,
 			text: text,
 			fileIds: opts?.file ? [opts?.file.id] : undefined,
 			cw: opts?.cw,
 			renoteId: opts?.renote
-		});
+		};
+
+		// DM以外は普通に返信し、DMの場合はDMで返信する
+		if (this.note.visibility != 'specified') {
+			return await this.ai.post(postData);
+		} else {
+			return await this.ai.sendMessage(this.userId, postData);
+		}
 	}
 
 	@bindThis
