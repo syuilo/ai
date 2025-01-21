fe6d17cdb00e7eb0a21e5f7fca8a28c66ab73991
L̷O̷N̷D̷O̷N̷少林 <22037550+Shivawkes@users.noreply.github.com>
2025-01-21 10:53:38 -0500

    index.ts

--- src/modules/follow/index.ts
+++ src/modules/follow/index.ts
@@ -14,7 +14,7 @@ export default class extends Module {
 
 	@bindThis
 	private async mentionHook(msg: Message) {
-		if (msg.text && msg.includes(['フォロー', 'フォロバ', 'follow me'])) {
+		if (msg.text && msg.includes(['Follow', 'Follow back', 'follow me'])) {
 			if (!msg.user.isFollowing) {
 				this.ai.api('following/create', {
 					userId: msg.userId,
